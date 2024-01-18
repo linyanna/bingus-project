fd1fb4f17b8262baf0c3d4c0ac359c5fdc0512e5
Yanna Lin <linyanna@yahoo.com>
2024-01-18 12:59:22 -0500

    initial setup with supabase

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import { createClient } from "@supabase/supabase-js";
+
+// Supabase setup referenced from: https://supabase.com/docs/guides/getting-started/quickstarts/reactjs
+
+const url = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const key = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!url || !key) {
+  throw new Error('Environment variables PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_ANON_KEY are required');
+}
+
+const supabase = createClient(url, key);
+
+function App() {
+  interface Country {
+    name: string;
+  }
+  
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    if (data) {
+      setCountries(data);
+    }
+  }
+
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.name}>{country.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App
