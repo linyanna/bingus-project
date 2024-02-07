6f4d4486a1e882fe76278e0e1261c7315ac04923
Brandon-Yip2 <73593739+Brandon-Yip2@users.noreply.github.com>
2024-02-07 12:16:36 -0500

    supabase auth-ui connection with email signup. Oauth not implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react'
-import { createClient } from "@supabase/supabase-js";
+import './index.css';
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Session } from '@supabase/supabase-js';
 
-// Supabase setup referenced from: https://supabase.com/docs/guides/getting-started/quickstarts/reactjs
 
 const url = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 const key = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
@@ -12,31 +15,49 @@ if (!url || !key) {
 
 const supabase = createClient(url, key);
 
-function App() {
-  interface Country {
-    name: string;
-  }
-  
-  const [countries, setCountries] = useState<Country[]>([]);
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
-    getCountries();
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
   }, []);
 
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    if (data) {
-      setCountries(data);
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    interface Country {
+      name: string;
     }
+  
+    const [countries, setCountries] = useState<Country[]>([]);
+  
+    useEffect(() => {
+      getCountries();
+    }, []);
+  
+    async function getCountries() {
+      const { data } = await supabase.from("countries").select();
+      if (data) {
+        setCountries(data);
+      }
+    }
+
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
+    );
   }
-
-  return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
-  );
 }
-
-export default App
