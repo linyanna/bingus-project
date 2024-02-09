c80825ae577ed46202d48ef9c2c02f0e1ab52cb1
Yanna Lin <linyanna@yahoo.com>
2024-02-09 14:41:57 -0500

    refactored routing logic, fixed signin to demo

--- src/components/SqlEditor.tsx
+++ src/components/SqlEditor.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // Sql.js config: https://github.com/sql-js/react-sqljs-demo/blob/master/src/App.js
 import initSqlJs from "sql.js";
-import sqlWasm from "../../../node_modules/sql.js/dist/sql-wasm.wasm?url"; // Required to let webpack 4 know it needs to copy the wasm file to our assets
-import SqlRepl from './SqlRepl';
+import sqlWasm from "../../node_modules/sql.js/dist/sql-wasm.wasm?url"; // Required to let webpack 4 know it needs to copy the wasm file to our assets
+import SqlRepl from "./SqlRepl";
+import "../styles/sqlEditor.css";
 
 // Database type from sql.js
 // TODO: define interface with methods
@@ -21,8 +22,10 @@ function SqlEditor() {
         const sqlPromise = await initSqlJs({ locateFile: () => sqlWasm });
         // Using a dummy database from: https://www.sqlitetutorial.net/sqlite-sample-database/
         // ex table names include playlists, artists, customers
-        const dataPromise = fetch("https://lynhjymnmasejyhzbhwv.supabase.co/storage/v1/object/public/chinook_db/chinook.db?t=2024-02-08T08%3A17%3A41.783Z").then(res => res.arrayBuffer());
-        const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+        const dataPromise = fetch(
+          "https://lynhjymnmasejyhzbhwv.supabase.co/storage/v1/object/public/chinook_db/chinook.db?t=2024-02-08T08%3A17%3A41.783Z"
+        ).then((res) => res.arrayBuffer());
+        const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
         setDb(new SQL.Database(new Uint8Array(buf)));
       } catch (err) {
         setError(err as string);
@@ -31,9 +34,24 @@ function SqlEditor() {
     fetchSqlData();
   }, []);
 
-  if (error) return <pre>{error.toString()}</pre>;
-  else if (!db) return <pre>Loading...</pre>;
-  else return <SqlRepl db={db} />;
+  if (error)
+    return (
+      <div className='sqlEditor'>
+        <pre>{error.toString()}</pre>
+      </div>
+    );
+  else if (!db)
+    return (
+      <div className='sqlEditor'>
+        <pre>Loading...</pre>
+      </div>
+    );
+  else
+    return (
+      <div className='sqlEditor'>
+        <SqlRepl db={db} />
+      </div>
+    );
 }
 
 export default SqlEditor;
--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import LandingPage from './components/LandingPage.tsx';
-import Navbar from './components/Navbar.tsx';
-import Dashboard from './components/Dashboard.tsx';
-import Signup from './components/Signup.tsx';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { createClient } from '@supabase/supabase-js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/index.css";
 
-
-const url = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const key = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
-
-const App: React.FC = () => {
-  const location = useLocation();
-  const supabase = createClient(url, key);
-  
-  //can add navbar to other pages
-  const navbarPaths = ["/dashboard"];
-
-  const isNavbarVisible = navbarPaths.includes(location.pathname);
-
-  return (
-    <div>
-      {isNavbarVisible && <Navbar />} {}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/signup" element={<Signup supabaseClient={supabase}/>} />
-      </Routes>
-    </div>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
