764106ddda299242c3890aedd8cb52e63ce10692
[ripeliz] <elizabethchoy02@gmail.com>
2024-02-23 13:26:05 -0500

    i hope this works

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import Profile from "./Profile";
 import SqlEditor from "./SqlEditor";
 import Brief from "./Brief";
-import Guide from "./Guide"
+import Guide from "./Guide";
 import Results from "./Results";
 import { Tab } from "./Navbar"; 
-import FileContainer from "./FileContainer";
 import '../styles/dashboard.css'; 
 
 interface DashboardProps {
@@ -14,16 +13,14 @@ interface DashboardProps {
 
 const Dashboard: React.FC<DashboardProps> = ({ activeTab }) => {
   return (
-    <div className="parent">
-    <FileContainer>
-        <div className="nav">
-          {activeTab === Tab.BRIEF && <Brief />}
-          {activeTab === Tab.SQL && <SqlEditor />}
-          {activeTab === Tab.RESULTS && <Results />}
-          {activeTab === Tab.GUIDE && <Guide />}
-          {activeTab === Tab.PROFILE && <Profile />}
-        </div>
-      </FileContainer>
+    <div className="filecontainer">
+      
+        {activeTab === Tab.BRIEF && <Brief />}
+        {activeTab === Tab.SQL && <SqlEditor />}
+        {activeTab === Tab.RESULTS && <Results />}
+        {activeTab === Tab.GUIDE && <Guide />}
+        {activeTab === Tab.PROFILE && <Profile />}
+      
     </div>
   );
 };
