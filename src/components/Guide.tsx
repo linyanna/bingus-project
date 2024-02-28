967cb2c9fd6a096e1a9569d1c0d1a5a5b712c70d
Scott Barcomb <scott.barcomb@ufl.edu>
2024-02-28 16:20:33 -0500

    added overflow scroll bar to guide

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -3,17 +3,39 @@ import Collapsible from "./Collapsible/Collapsible";
 function Guide() {
     return (
     <div className="Guide">
-        <Collapsible label = "Introduction">
-            <h1>Prerequisite</h1>
+        <Collapsible label = "What is SQL?">
             <p>
-                Helloooooooo you should prob know some SQL to play this game
+                SQL stands for <b>Structured Query Language</b> and is a domain-specific language
+                used to manage data, especially in a relational database management system. SQL is
+                used to access and manipulate data stored in tables. Each table has columns that
+                contain different attributes, and has rows that contain different records. SQL
+                commands are case insenstive, meaning 'SELECT', 'select', and 'Select' all have
+                the same meaning.
             </p>
         </Collapsible>
         <hr />
-        <Collapsible label = "Goals">
-            <h1>Goals</h1>
+        <Collapsible label = "SQL Rules">
             <p>
-                This guide will teach you the basic commands of SQL to hopefully get you started!
+                SQL syntax is made up of various statements and queries which are composed of identifiers
+                and clauses. All SQL statements must end with a semicolon ';' but can be composed over
+                multiple lines. Statements may have a persistent effect on data, whereas queries retrieve
+                data based on specific criteria.
+            </p>
+        </Collapsible>
+        <hr />
+        <Collapsible label = "SELECT all from table">
+            <p>
+                To fetch the entire table or all fields in the table:
+                SELECT * FROM table_name;
+                Here, the asterisk '*' represents all attributes in the table.
+            </p>
+        </Collapsible>
+        <hr />
+        <Collapsible label = "test">
+            <p>
+                To fetch the entire table or all fields in the table:
+                SELECT * FROM table_name;
+                Here, the asterisk '*' represents all attributes in the table.
             </p>
         </Collapsible>
     </div>
