48fced670c74b8a5be68c825edb80e2bfecbf48a
scottbarcomb <scott.barcomb@ufl.edu>
2024-04-11 19:32:55 -0400

    added first part of command checking

--- src/components/SqlInput.tsx
+++ src/components/SqlInput.tsx
@@ -8,7 +8,7 @@ type Database = any;
 /**
  * A simple SQL read-eval-print-loop
  * @param {{db: import("sql.js").Database}} props
- */
+*/
 
 // Function to update local storage with the current database state
 function updateLocalStorage(database: Database) {
@@ -31,11 +31,41 @@ function SqlInput({ db }: { db: Database }) {
     try {
       // The sql is executed synchronously on the UI thread.
       // You may want to use a web worker here instead
+
+      handleCommand(0, command);
+
       setResults(db.exec(command)); // an array of objects is returned
       setError(null);
 
       // Update local storage with the current database state
       updateLocalStorage(db);
+    } catch (err) {
+      // exec throws an error when the SQL statement is invalid
+      setError(err as string);
+      setResults([]);
+    }
+  }
+
+  // this function will lex/parse the SQL statement based off of the input paramater for where in the story the player is
+  function handleCommand(crosscheck: number, command: any) {
+    try {
+      if (!command?.endsWith(";")) {
+        throw new Error("Commands must end with a semicolon!");
+      }
+  
+      const str = command.toLowerCase();
+
+      // crosscheck is 0 -> check for first command (SELECT * FROM inventory;)
+      if (crosscheck == 0) {
+        const regex = /([a-z\*]+)/g;
+        const array = [...str.matchAll(regex)];
+    
+        // Check that each component of array is correct (parsing)
+        if (array[0][0] != 'select') throw new Error("Hint: use the SELECT statement.");
+        if (array[1][0] != '*') throw new Error("Hint: use the '*' character to select all.");
+        if (array[2][0] != 'from') throw new Error("Hint: use the FROM statement when trying to grab data from a table");
+        if (array[3][0] != 'inventory') throw new Error("Hint: we are trying to look through our 'inventory'");
+      }
     } catch (err) {
       // exec throws an error when the SQL statement is invalid
       setError(err as string);
